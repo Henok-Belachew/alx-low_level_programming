c844df770fe842d1548374e7d50e4d5bdab9af04
Henok.B <learning4myfuture@gmail.com>
2022-09-13 15:00:32 +0300

    first shot

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * add - Adds two values
+  * @s1: The first number to be computed
+  * @s2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int s1, int s2)
+{
+	return (s1 + s2);
+}
+
