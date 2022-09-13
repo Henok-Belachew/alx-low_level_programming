9bed8d0a9f5c1566c91447e12d69d4a73a484033
Henok.B <learning4myfuture@gmail.com>
2022-09-13 16:34:13 +0300

    C: Project-3

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add -> adds two number
+* @x : param1
+* @y : param2
+* Return: x + y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _islower -> checks if the character is lowercase
+* @c: a charcter argument
+* Return: returns 1 and 0 depending on condition
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
