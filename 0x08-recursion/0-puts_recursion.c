51cb54a6d34639ffcc1bd41c0f4ba8e47fa5fde6
Henok.B <learning4myfuture@gmail.com>
2022-09-28 18:53:31 +0300

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
