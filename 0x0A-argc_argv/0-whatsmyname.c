9e5bb790f9412e0c5a6e92984191658f55332a4d
Henok Belachew <learning4myfuture@gmail.com>
2022-09-30 05:23:33 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - main function that returns the program filename
+ * @argc: number of argument
+ * @argv: character string
+ *
+ * Return - returns nothing
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
