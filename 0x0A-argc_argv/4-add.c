0c075df0fb671bfd649e7285bdc5651dec8b9cf6
Henok.B <learning4myfuture@gmail.com>
2022-09-30 20:36:40 +0300

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * isInteger - checks if the argument is integer or not
+ * @s: array of character
+ * Return: 0 || 1
+ */
+
+int isNotInteger(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * main - adds all the given positive numbers
+ * @argc: number of argument passed
+ * @argv: array to store arguments
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int sum = 0;
+
+	while (--argc >= 0)
+
+	{
+		if (isNotInteger(argv[argc]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[argc]);
+	}
+	printf("%i\n", sum);
+	return (0);
+}
