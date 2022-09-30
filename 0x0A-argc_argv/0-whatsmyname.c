a3cf1365e14182079e61f800647dbe4b2ef8a707
Henok.B <learning4myfuture@gmail.com>
2022-09-30 15:38:51 +0300

    Updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
-
 /**
- * main - main function that returns the program filename
- * @argc: number of argument
- * @argv: character string
- *
- * Return: returns nothing
- */
-
-int main(int argc __attribute__((unused)), char *argv[])
+* main -  writes program name
+* @argc: arguement count
+* @argv: array of pointers to CLI arguement
+* Return: 0
+**/
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
