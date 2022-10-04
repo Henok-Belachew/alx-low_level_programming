93001b88d2e113b984841bcdbcf89603a7148042
Henok Belachew <learning4myfuture@gmail.com>
2022-10-04 01:01:15 -0700

    Task o

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars at runtime
+ * @size: the size of the array
+ * @c: the char that is going to be assigned for the array
+ * Return: NULL if size = 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	unsigned int i;
+
+	str = malloc(sizeof(str[0]) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+	{
+		str[i] = c;
+	}
+
+	return (str);
+}
