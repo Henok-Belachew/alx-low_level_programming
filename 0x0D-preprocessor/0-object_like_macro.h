6c7e2b434304bf93fd84d3588b746c0cf4283084
Henok.B <learning4myfuture@gmail.com>
2022-10-10 23:35:53 +0300

    C: Project 12

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef _OBJECT_LIKE_MACRO_
+#define _OBJECT_LIKE_MACRO_
+
+#define SIZE 1024
+
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
+
+
+#define SUM(x, y) ((x) + (y))
+
+
+#endif
