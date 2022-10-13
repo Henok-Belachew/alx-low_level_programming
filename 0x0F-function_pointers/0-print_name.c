f0e5e9c239b102d511a7d22ec646fe625548950a
Henok Belachew <learning4myfuture@gmail.com>
2022-10-13 04:37:01 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name passed using callback function
+ * @name: name to be print
+ * @f: function to print passed name
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
