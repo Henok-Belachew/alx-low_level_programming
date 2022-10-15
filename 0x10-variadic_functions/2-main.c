4263566f5fd7e2dab3cc2e201a075a62d3bc465e
Henok Belachew <learning4myfuture@gmail.com>
2022-10-15 05:21:36 -0700

    Task 2

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    print_strings(", ", 2, "Jay", "Django");
+	print_strings("", 2, "jay", "Henok", 3);
+	        return (0);
+
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings followed by a new line
+ * @separator: string to sepaarate passed strings
+ * @n: number of arguments passed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	count i = 0;
+	va_list theList;
+
+	va_start(theList, n);
+
+	for (; i < n; i++)
+	{
+		printf("%s", va_arg(theList, char *));
+
+		if (separator)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(theList);
+}
