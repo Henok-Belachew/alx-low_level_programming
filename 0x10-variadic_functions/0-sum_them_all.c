64235241a49446121a70cfffdcbf3bad1179dc03
Henok Belachew <learning4myfuture@gmail.com>
2022-10-15 03:57:07 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	count i = 0;
+
 	if (n == 0)
 	{
 		return (0);
 	}
 	va_list theList;
-	va_start (theList,n);
+
+	va_start(theList, n);
 	for (; i < n; i++)
 	{
-		sum += var_arg(theList,int);
+		sum += var_arg(theList, int);
 	}
 	va_end(theList);
 	return (sum);
